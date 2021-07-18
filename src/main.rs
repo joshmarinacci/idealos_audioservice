8a644edf58cabe43dcbe32f56126e3a3f6714f21
josh <joshua@marinacci.org>
2021-07-18 12:05:39 -0700

    switch to using JSON over websockets

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,17 @@ use websocket::{OwnedMessage};
 use websocket::ClientBuilder;
 use std::thread;
 use std::collections::HashMap;
+use serde_json::{json};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+struct AudioMessage {
+    id:String,
+    #[serde(rename = "type")]
+    pub type_:String,
+    command:String,
+    resource:String,
+}
 
 const ID: &str = "AN_ID";
 
@@ -24,11 +34,17 @@ pub fn main() -> Result<(),String> {
     let (_stream, handle) = OutputStream::try_default().unwrap();
 
     let mut sinks:HashMap<String,Sink> = HashMap::new();
-    // let sink = Sink::try_new(&handle).unwrap();
-    // let sink2 = Sink::try_new(&handle).unwrap();
+
+    sender.send_message(&OwnedMessage::Text(json!(AudioMessage {
+                            id: "".to_string(),
+                            type_: "AUDIO".to_string(),
+                            command: "connected".to_string(),
+                            resource: "audio".to_string()
+                        }).to_string()));
 
     let receive_loop = thread::spawn(move || {
         // Receive loop
+        println!("waiting for messages");
         for message in receiver.incoming_messages() {
             let message = match message {
                 Ok(m) => m,
@@ -45,33 +61,52 @@ pub fn main() -> Result<(),String> {
                 }
                 OwnedMessage::Text(str) => {
                     println!("got message {:?}",str);
-                    let parts:Vec<&str> = str.split(":").collect();
-                    if parts[0].eq("load") {
-                        let file = BufReader::new(File::open(parts[1]).unwrap());
+                    // let v: Value = serde_json::from_str(str.as_str())?;
+                    let msg:AudioMessage = serde_json::from_str(str.as_str()).unwrap();
+                    println!("incoming message {:?}",msg);
+                    if msg.command.eq("load") {
+                        let file = BufReader::new(File::open(msg.resource).unwrap());
                         let source = Decoder::new(file).unwrap();
                         let sink = Sink::try_new(&handle).unwrap();
                         sink.append(source);
                         sink.pause();
-
                         sinks.insert(ID.to_string(), sink);
-                        let msg = format!("loaded:{:}",ID);
-                        sender.send_message(&OwnedMessage::Text(msg));
+                        sender.send_message(&OwnedMessage::Text(json!(AudioMessage {
+                            id: "".to_string(),
+                            type_: "AUDIO".to_string(),
+                            command: "played".to_string(),
+                            resource: ID.to_string()
+                        }).to_string()));
+                        continue
                     }
-                    if parts[0].eq("play") {
-                        if let Some(sink) = sinks.get(parts[1]) {
+                    if msg.command.eq("play") {
+                        if let Some(sink) = sinks.get(msg.resource.as_str()) {
                             sink.play();
-                            let msg = format!("played:{:}",ID);
-                            sender.send_message(&OwnedMessage::Text(msg));
+                            let msg:AudioMessage = AudioMessage {
+                                id: "".to_string(),
+                                type_: "AUDIO".to_string(),
+                                command: "played".to_string(),
+                                resource: ID.to_string()
+                            };
+                            // let msg = format!("played:{:}",ID);
+                            sender.send_message(&OwnedMessage::Text(json!(msg).to_string()));
+                            continue
                         }
                     }
-                    if parts[0].eq("pause") {
-                        if let Some(sink) = sinks.get(parts[1]) {
+                    if msg.command.eq("pause") {
+                        if let Some(sink) = sinks.get(msg.resource.as_str()) {
                             sink.pause();
-                            let msg = format!("paused:{:}",ID);
-                            sender.send_message(&OwnedMessage::Text(msg));
+                            let msg:AudioMessage = AudioMessage {
+                                id: "".to_string(),
+                                type_: "AUDIO".to_string(),
+                                command: "paused".to_string(),
+                                resource: ID.to_string()
+                            };
+                            sender.send_message(&OwnedMessage::Text(json!(msg).to_string()));
+                            continue
                         }
                     }
-                    if parts[0].eq("exit") {
+                    if msg.command.eq("exit") {
                         break;
                     }
                 }
@@ -82,6 +117,7 @@ pub fn main() -> Result<(),String> {
     });
     let _ = receive_loop.join();
 
+    println!("audio service ending");
     Ok(())
 
 }
